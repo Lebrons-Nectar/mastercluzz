0b75855b46e269877f9f366bf3cc78b05354855a
francislaw-bomba <119966985+francislaw-bomba@users.noreply.github.com>
2025-03-09 21:10:42 +0100

    hodzenie

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, CharacterMovement.playerSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public static float playerSpeed = 3.3f;
+    public bool isAlive = true;
+    public SpriteRenderer spriteRenderer;
+    public static int playerHealth = 3;
+    public GameObject player;
+
+    void Start()
+    {
+       // logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<logicScript>();
+        transform.position = new Vector3(transform.position.x, transform.position.y, 1.0f);
+        playerHealth = 3;
+    }
+
+    void Update()
+    {
+        if (isAlive == true)
+        {
+            Vector3 playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+            transform.position = transform.position + playerInput.normalized * playerSpeed * Time.deltaTime;
+        }
+
+        if (playerHealth <= 0)
+        {
+            isAlive = false;
+            Destroy(player);
+           // logic.gameOver();
+        }
+    }
+}
